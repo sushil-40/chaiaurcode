31f6b0c9edbb3b7e266298780129cb992efd84dc
Sushil <sushildangoriya40@gmail.com>
2025-08-10 21:00:40 +1000

    adding chaiaurcode

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const handleOnIncrement = () => {
+    setCount((prevCounter) => prevCounter + 1);
+    setCount((prevCounter) => prevCounter + 1);
+    setCount((prevCounter) => prevCounter + 1);
+    setCount((prevCounter) => prevCounter + 1);
+
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+  };
+  const handleOnDecrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <h3>Counter App!</h3>
+      <h4>{count}</h4>
+      <button onClick={handleOnIncrement}>Increment</button>
+      <span> </span>
+      <button onClick={handleOnDecrement}>Decrement</button>
+    </>
+  );
+}
+
+export default App;
