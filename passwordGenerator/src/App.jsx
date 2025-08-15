e0fdf37bc1aa138b694bd5c23833104b1ec8d67c
Sushil <sushildangoriya40@gmail.com>
2025-08-16 08:09:29 +1000

    complete password generator app

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -6,6 +6,10 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  // useRef hook
+
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -16,11 +20,20 @@ function App() {
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 101);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-600">
@@ -35,9 +48,10 @@ function App() {
             value={password}
             className="outline-none w-full py-2 px-3 bg-gray-500 text-white mb-5"
             readOnly
+            ref={passwordRef}
           />
           <button
-            // onClick={copyToClipboard}
+            onClick={copyPasswordToClipboard}
             className="bg-blue-700 text-white px-4 shrink-0 mb-5  ms-3 hover:bg-blue-800"
           >
             Copy
