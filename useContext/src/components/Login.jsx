7db33dfa9045763d1686132c2c3d8e787c601f4f
Sushil <sushildangoriya40@gmail.com>
2025-08-17 10:51:04 +1000

    adding userContext hook implementation

---
+++ useContext/src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/userContext";
+
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser(userName, password);
+    console.log(userName, password);
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder="username"
+      />{" "}
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ useContext/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import UserContext from "../context/userContext";
+
+const Profile = () => {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <div>Please Login</div>;
+  return <div>Welcome! {user.userName}</div>;
+};
+
+export default Profile;
