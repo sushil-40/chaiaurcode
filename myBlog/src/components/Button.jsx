27a83c82bc5929a864353acddb730d809649727a
Sushil <sushildangoriya40@gmail.com>
2025-09-04 09:10:14 +1000

    adding Post component

--- myBlog/src/components/Button.jsx
+++ myBlog/src/components/Button.jsx
@@ -10,7 +10,7 @@ const Button = ({
 }) => {
   return (
     <button
-      className={`px-4 py-2 rounded-lg ${className} ${bgColor} ${textColor} `}
+      className={`px-4 py-2 rounded-lg ${className} ${bgColor} ${textColor}${type} `}
       {...props}
     >
       {children}
---
+++ myBlog/src/pages/EditPost.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Container, PostForm } from "../components";
+import { useNavigate, useParams } from "react-router-dom";
+import appWriteService from "../services/appWriteService";
+const EditPost = () => {
+  const [post, setPost] = useState(null);
+  const navigate = useNavigate();
+  const { slug } = useParams();
+
+  useEffect(() => {
+    if (slug) {
+      appWriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  ) : null;
+};
+
+export default EditPost;
